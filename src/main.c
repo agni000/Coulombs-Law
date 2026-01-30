ea7273537175cfa9902fc03f314e4fbd814334dc
agni000 <j.juniorpires98@gmail.com>
2026-01-29 23:44:33 -0300

    refactor: calculation mistakes

--- src/main.c
+++ src/main.c
@@ -7,10 +7,11 @@
 #define WIDTH 800
 #define HEIGHT 600
 #define FPS 30
-#define MAX_CHARGES 20
-#define CHARGE_RADIUS 20
+#define MAX_CHARGES 5
+#define CHARGE_RADIUS 10
 #define CONST_K 900000.0f
 #define CHARGE_MASS 0.9f 
+#define DAMPING 0.98f 
 
 typedef struct {
   float x; 
@@ -77,6 +78,12 @@ Charge CreateCharge(Vector pos, Vector vel, Vector frc, float chr, Color col) {
 /*@brief Apply the formula F = (K*(q1*q2)/(r^2))*û 
   @param The charges vector*/
 void CoulombsLaw(Charge *charges) {
+  /*reset forces for each calculation*/
+  for (size_t i = 0; i < MAX_CHARGES; i++) {
+    charges[i].force.x = 0.0f;
+    charges[i].force.y = 0.0f;
+  }
+
   for (size_t i = 0; i < MAX_CHARGES; i++) {
     for (size_t j = i + 1; j < MAX_CHARGES; j++) {
       float dx = charges[j].position.x - charges[i].position.x; 
@@ -85,7 +92,7 @@ void CoulombsLaw(Charge *charges) {
       float distance = (float)sqrt((double)(dx*dx + dy*dy)); 
       
       /*avoid division by zero*/
-      if (distance <= 0) distance = 1;
+      if (distance < 5) distance = 9999;
 
       /*unit vectors*/ 
       float ux = dx / distance; 
@@ -100,7 +107,7 @@ void CoulombsLaw(Charge *charges) {
       charges[i].force.x += fx;
       charges[i].force.y += fy;
       charges[j].force.x -= fx; 
-      charges[i].force.y -= fy; 
+      charges[j].force.y -= fy; 
 
       printf("fx: %.3f\nfy: %.3f\n", fx, fy);
     }
@@ -115,14 +122,19 @@ void UpdateCharges(Charge *charges, float dt) {
     float ax = charges[i].force.x / CHARGE_MASS; 
     float ay = charges[i].force.y / CHARGE_MASS; 
     
-    /* a = dv / dt then dv = a / dt */
-    charges[i].velocity.x += ax / dt; 
-    charges[i].velocity.y += ay / dt; 
+    /* (a = (dv / dt)) then (dv = (a * dt)) */
+    charges[i].velocity.x += ax * dt; 
+    charges[i].velocity.y += ay * dt;
 
-    /* v = ds / dt then ds = v / dt */ 
-    charges[i].position.x += charges[i].velocity.x / dt; 
-    charges[i].position.y += charges[i].velocity.y / dt;
-    
+    /* DAMPING test */
+    charges[i].velocity.x *= DAMPING; 
+    charges[i].velocity.y *= DAMPING; 
+
+    /* (v = (ds / dt)) then (ds = (v * dt)) */ 
+    charges[i].position.x += charges[i].velocity.x * dt; 
+    charges[i].position.y += charges[i].velocity.y * dt;
+   
+    /* boundary conditions */
     if (charges[i].position.x < CHARGE_RADIUS || charges[i].position.x > WIDTH - CHARGE_RADIUS) {
         charges[i].velocity.x *= -0.8f;
         charges[i].position.x = (charges[i].position.x < CHARGE_RADIUS) ? CHARGE_RADIUS : WIDTH - CHARGE_RADIUS;
